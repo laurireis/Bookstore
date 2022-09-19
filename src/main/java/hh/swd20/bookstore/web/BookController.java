6b45e0dbe8817d668eb0270614271dfbbc93104d
Lauri Reis <reislauri@gmail.com>
2022-09-19 11:57:52 +0300

    Added listing

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -1,18 +1,23 @@
 package hh.swd20.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	
-	@RequestMapping(value="/index", method = RequestMethod.GET)
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping(value="/booklist", method = RequestMethod.GET)
 	public String getBooks(Model model) {
-		return "welcome";
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 	
 }
